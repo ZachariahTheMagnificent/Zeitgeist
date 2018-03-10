280a845c2e0946a7ae843c2e40714943fd891855
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-03-10 21:45:39 +0800

    Made documentation template
    A template on how documentation of the code will be done. Documentation on the elements of the code like classes, variables, functions, namespaces and even files will be done by making comments with this template at the top of the file or before the declaration.

--- glfw/source/glfw/Application.hpp
+++ glfw/source/glfw/Application.hpp
@@ -20,6 +20,21 @@ namespace glfw
 		void poll_events ( );
 
 	private:
+		/*************************************************************************/
+		/**
+		A function that can be overridden by the derived class to respond to an
+		event where a joystick is connected.
+
+		This function is usually called on poll_events ( ). However, it is
+		possible for certain systems to call it asynchronously and in parallel to
+		the program's execution.
+
+		@param id
+		The id of the joystick.
+
+		@return
+		Nothing.
+		**************************************************************************/
 		virtual void on_joystick_connected ( const std::size_t id );
 		virtual void on_joystick_disconnected ( const std::size_t id );
 	};
